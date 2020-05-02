d2de0a8e88ae896c0bb175188a8bcf88eeacbc2a
Andres Hernandez <aahrodriguez@gmail.com>
2020-05-02 13:28:24 -0300

    Api with v1

--- app/controllers/api/v1/carros_controller.rb
+++ app/controllers/api/v1/carros_controller.rb
@@ -3,4 +3,18 @@ class Api::V1::CarrosController < Api::V1::BaseController
   def index
     @carros = policy_scope(Carro)
   end
+
+  def show
+    @carro = Carro.find(params[:id])
+    authorize @carro
+
+  end
+
+  private
+
+    def set_carro
+      @carro = Carro.find(params[:id])
+      authorize @carro
+    end
+
 end
--- app/policies/carro_policy.rb
+++ app/policies/carro_policy.rb
@@ -4,4 +4,8 @@ class CarroPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def show?
+    true
+  end
 end
